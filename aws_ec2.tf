06429ed6fa2a1e49e4a7261dfeb643047bcc78a1
muneerabdul <32707926+muneerabdul@users.noreply.github.com>
2019-05-24 18:25:12 +0530

    Updated with Demo Changes

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region     = var.region
 }
 
-resource "aws_instance" "mytrial" {
+resource "aws_instance" "mynewdemo" {
   ami           = "ami-07a0c6e669965bb7c"
   instance_type = "t2.micro"
 }
