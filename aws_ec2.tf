5c36fc765fc26f388bf6640b8ac9f0dd242b82f6
muneerabdul <32707926+muneerabdul@users.noreply.github.com>
2019-05-24 17:24:10 +0530

    Updates

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,6 +1,9 @@
-provider "aws" {}
-
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
 resource "aws_instance" "mytrial" {
-  ami           = "ami-05af84768964d3dc0"
+  ami           = "ami-07a0c6e669965bb7c"
   instance_type = "t2.micro"
 }
